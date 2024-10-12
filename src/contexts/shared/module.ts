016c3c0ad073630a90ba12d8328bfd7b307dc842
Waldo Sobrino <waldo2810@gmail.com>
2024-10-11 20:33:48 -0500

    feat: setup typeorm config

--- src/contexts/shared/module.ts
+++ src/contexts/shared/module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'contacts',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
   ],
 })
 export class SharedModule {}
