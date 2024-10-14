645f7db8f90b7f0ef130730f05db77e8fb0cf5c4
Waldo Sobrino <waldo2810@gmail.com>
2024-10-13 21:11:43 -0500

    feat: doc health controller

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Post, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 
+@ApiTags('health')
 @Controller()
 export class HealthController {
   @Get()
+  @ApiOperation({
+    summary: 'Health check',
+    description: 'Check if the service is up and running',
+  })
   findAll(@Res() res: Response) {
     res.status(HttpStatus.OK).json({ message: 'ok' });
   }
